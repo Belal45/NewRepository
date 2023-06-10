b7616319e36e4fb2e311292d2876a05649a12a43
belal ahmad <belalahm092@gmail.com>
2023-06-10 20:14:17 +0530

    bug fix

--- MyProject/src/com/programs/Test.java
+++ MyProject/src/com/programs/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("clone and push code in repository");
+		System.out.println("clone and push code in repository..");
 
 	}
 
