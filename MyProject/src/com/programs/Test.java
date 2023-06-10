aac4c2f61d3b94d713607acbc052d6180df34fe3
belal ahmad <belalahm092@gmail.com>
2023-06-10 19:56:54 +0530

    third commit

--- MyProject/src/com/programs/Test.java
+++ MyProject/src/com/programs/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("welcome");
+		System.out.println("welcome once again");
 
 	}
 
